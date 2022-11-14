54cee2b33e014ca8b13a13f0eaff9264421fb16c
JungKata <97729333+JungKata@users.noreply.github.com>
2022-11-14 22:08:08 +0100

    fuvar.csv beillesztese program letrehozása fuvarPerformat letrehozása - fajl konvertalasa megfelelo modon listaba helyzes konstruktor getter elkészitese

---
+++ src/FuvarPerformat.java
@@ -0,0 +1,51 @@
+public class FuvarPerformat {
+    private int taxiAzonosito;
+    private String indulasiIdo;
+    private int utazasiIdo;
+    private int megtettTavolsag;
+    private int vetelDij;
+    private int borravalo;
+    private String fizetesModja;
+
+    public FuvarPerformat(String sor) {
+        String[] s = sor.split(";");
+        this.taxiAzonosito = Integer.parseInt(s[0]);
+        this.indulasiIdo = s[1];
+        this.utazasiIdo = Integer.parseInt(s[2]);
+        this.megtettTavolsag = Integer.parseInt(s[3]);
+        this.vetelDij = Integer.parseInt(s[4]);
+        this.borravalo = Integer.parseInt(s[5]);
+        this.fizetesModja = s[6];
+    }
+
+    public int getTaxiAzonosito() {
+        return taxiAzonosito;
+    }
+
+    public String getIndulasiIdo() {
+        return indulasiIdo;
+    }
+
+    public int getUtazasiIdo() {
+        return utazasiIdo;
+    }
+
+    public int getMegtettTavolsag() {
+        return megtettTavolsag;
+    }
+
+    public int getVetelDij() {
+        return vetelDij;
+    }
+
+    public int getBorravalo() {
+        return borravalo;
+    }
+
+    public String getFizetesModja() {
+        return fizetesModja;
+    }
+}
+
+
+
